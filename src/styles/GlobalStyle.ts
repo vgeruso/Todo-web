bd2c749874063cbfc4af25b6113d98b143be5163
vgeruso <victor.geruso@gmail.com>
2024-03-26 00:26:35 -0300

    [ADD] Global configuration

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #291334;
+    min-height: 100vh;
+  }
+`;
