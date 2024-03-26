7f9adda6c4fe149101e635ff61b76e1caa2d2f07
vgeruso <victor.geruso@gmail.com>
2024-03-26 00:26:19 -0300

    [ADD] functions access API

--- src/services/task.ts
+++ src/services/task.ts
@@ -30,6 +30,36 @@ export const addTask = async (data: any) => {
     });
 };
 
+export const getTaskById = async (id: string) => {
+  return await fetch(`${baseUrl}/task/${id}`, {
+    mode: 'cors',
+    method: 'GET',
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
+
+export const editTask = async (data: any) => {
+  return await fetch(`${baseUrl}/task/update`, {
+    mode: 'cors',
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
+
 export const deleteTask = async (taskId: any) => {
   return await fetch(`${baseUrl}/task/${taskId}`, {
     mode: 'cors',
