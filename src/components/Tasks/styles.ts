0c49767fed55e9341179809d3284ebb0d516fb89
vgeruso <victor.geruso@gmail.com>
2024-03-26 00:25:44 -0300

    [ADD] Tasks component

---
+++ src/components/Tasks/styles.ts
@@ -0,0 +1,22 @@
+import { styled } from 'styled-components';
+
+export const ContainerList = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #faf7f5;
+  border-radius: 10px;
+  margin: 10px;
+  flex-direction: column;
+  padding: 10px;
+`;
+
+export const ContainerInput = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #faf7f5;
+  border-radius: 10px;
+  margin: 10px;
+  padding: 10px;
+`;
